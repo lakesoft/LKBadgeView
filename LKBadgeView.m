a9b0c31b78a926f0596c9dd93edd222f2645c7e1
Hiroshi Hashiguchi <xcatsan@mac.com>
2011-09-29 12:03:35 +0900

    first release

--- LKBadgeView.m
+++ LKBadgeView.m
@@ -1,9 +1,23 @@
 //
-//  LKBadgeView.m
-//  LKBadgeView
+// Copyright (c) 2011 Hiroshi Hashiguchi
 //
-//  Created by Hashiguchi Hiroshi on 11/09/23.
-//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 //
 
 #import "LKBadgeView.h"
@@ -40,23 +54,26 @@
     self.displayinText = nil;
 }
 
-- (void)_setupDefaultWithOutline
+- (void)_setupDefaultWithoutOutline
 {
     self.textColor = [UIColor whiteColor];
     self.badgeColor = [UIColor grayColor];
+
     outline_ = NO;
+    outlineWidth_ = 2.0;
+    self.outlineColor = [UIColor colorWithWhite:0.65 alpha:1.0];
 
     [self _setupBasics];
 }
 
-- (void)_setupDefaultWithoutOutline
+- (void)_setupDefaultWithOutline
 {
     self.textColor = [UIColor grayColor];
     self.badgeColor = [UIColor whiteColor];
-    self.outlineColor = [UIColor colorWithWhite:0.65 alpha:1.0];
 
     outline_ = YES;
     outlineWidth_ = 3.0;
+    self.outlineColor = [UIColor colorWithWhite:0.65 alpha:1.0];
 
     [self _setupBasics];
 }
@@ -137,7 +154,7 @@
 - (id)init {
     self = [super init];
     if (self) {
-        [self _setupDefaultWithOutline];
+        [self _setupDefaultWithoutOutline];
     }
     return self;
 }
@@ -145,7 +162,7 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        [self _setupDefaultWithOutline];
+        [self _setupDefaultWithoutOutline];
     }
     return self;
 }
@@ -153,7 +170,7 @@
 - (id)initWithCoder:(NSCoder *)coder {
     self = [super initWithCoder:coder];
     if (self) {
-        [self _setupDefaultWithOutline];
+        [self _setupDefaultWithoutOutline];
     }
     return self;
 }
